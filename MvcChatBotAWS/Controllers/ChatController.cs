8c1d7f2cd5b5c035d8a692a1f2e3101498556afa
saraalvarex <alvarezalmacen19@gmail.com>
2023-06-01 13:44:56 +0200

    Add project files.

---
+++ MvcChatBotAWS/Controllers/ChatController.cs
@@ -0,0 +1,50 @@
+﻿using Amazon.LexRuntimeV2;
+using Amazon.LexRuntimeV2.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+public class ChatController: Controller
+{
+    private readonly AmazonLexRuntimeV2Client lexClient;
+
+    public ChatController()
+    {
+        lexClient = new AmazonLexRuntimeV2Client(); // Asegúrate de configurar las credenciales adecuadas
+    }
+
+    public IActionResult SendMessageToChatBot()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task SendMessageToChatBot(string userInput)
+    {
+        var request = new RecognizeTextRequest
+        {
+            BotId = "WBACPFOPCQ",
+            BotAliasId = "pruebas",
+            LocaleId = "es_ES",
+            //SessionId: Es un identificador único que representa una sesión de conversación con el bot.
+            //Debes generar un SessionId único para cada usuario o cada vez que inicies una nueva conversación.
+            //Puedes generar un SessionId de cualquier manera que sea conveniente para tu aplicación.Por ejemplo,
+            //puedes utilizar un identificador único generado por tu aplicación o una combinación de datos del usuario.
+            //Es importante que el SessionId sea único para cada sesión de conversación para mantener el seguimiento adecuado
+            //del contexto de la conversación.
+            SessionId = "163y68h",
+            Text = userInput
+        };
+
+        //var response = lexClient.RecognizeText(request);
+        var response = await lexClient.RecognizeTextAsync(request);
+
+        // Procesa la respuesta del chatbot según sea necesario
+        if (response != null && response.Messages.Count > 0)
+        {
+            foreach (var message in response.Messages)
+            {
+                Console.WriteLine("Respuesta del chatbot: " + message.Content);
+            }
+        }
+    }
+}
---
+++ MvcChatBotAWS/Program.cs
@@ -0,0 +1,40 @@
+using Amazon.LexRuntimeV2;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Registro del servicio de Amazon Lex
+builder.Services.AddSingleton<AmazonLexRuntimeV2Client>(sp =>
+{
+    var credentials = new Amazon.Runtime.BasicAWSCredentials("AKIASED2MTKXG6T5KROW", "Ouqf1RzTatug7bZmz6N/7AWh3zryzJRMggmUcjKb");
+    var config = new AmazonLexRuntimeV2Config
+    {
+        RegionEndpoint = Amazon.RegionEndpoint.USEast1 // Virginia
+    };
+
+    return new AmazonLexRuntimeV2Client(credentials, config);
+});
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
