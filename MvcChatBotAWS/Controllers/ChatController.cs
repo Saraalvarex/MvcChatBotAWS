d2cedd7a2792cfb1f82921023b4b085c91037f6b
saraalvarex <alvarezalmacen19@gmail.com>
2023-06-12 22:25:40 +0200

    ajax

--- MvcChatBotAWS/Controllers/ChatController.cs
+++ MvcChatBotAWS/Controllers/ChatController.cs
@@ -17,6 +17,11 @@ public class ChatController : Controller
     {
         lexClient = new AmazonLexRuntimeV2Client(); // Asegúrate de configurar las credenciales adecuadas
     }
+    public IActionResult Prueba()
+    {
+        return View();
+    }
+
     //EJEMPLOS PREGUNTAS CHATBOT:
     //Mas vueltas
     //Mas curvas
@@ -24,15 +29,16 @@ public class ChatController : Controller
     //Piloto o equipo con mas puntos
     //La mas dificil es Monaco
     //QUESTION: ¿Cuál es el circuito más complicado de F1?
-    public IActionResult Index()
+    public IActionResult _Chat()
     {
-        return View();
+        return Json(botMessages);
     }
 
     [HttpPost]
-    public async Task<IActionResult> Index(string userInput)
+    public async Task<IActionResult> _Chat(string userInput)
     {
-        if (botMessages == null)
+        // Lógica para procesar la entrada del usuario y obtener la respuesta del bot
+        if(botMessages == null)
         {
             botMessages = new List<Assistant>();
         }
@@ -51,43 +57,37 @@ public class ChatController : Controller
             {
                 string url = "http://ergast.com/api/f1/current/driverstandings.json";
                 HttpResponseMessage response = await client.GetAsync(url);
-                string responseBody = await response.Content.ReadAsStringAsync();
-
-                // Deserializar la respuesta JSON
-                JObject json = JObject.Parse(responseBody);
-
-                // Obtener la lista de pilotos
-                List<JToken> driverStandings = json["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"].Children().ToList();
-
-                // Encontrar el piloto con más puntos
-                int maxPoints = 0;
-                string driverName = "";
-
-                foreach (JToken driver in driverStandings)
-                {
-                    int points = int.Parse(driver["points"].ToString());
-                    if (points > maxPoints)
-                    {
-                        maxPoints = points;
-                        driverName = driver["Driver"]["givenName"].ToString() + " " + driver["Driver"]["familyName"].ToString();
-                    }
-                }
                 if (response.IsSuccessStatusCode)
                 {
-                    string jsonResult = await response.Content.ReadAsStringAsync();
-                    // Procesar los datos JSON de los resultados de la clasificación aquí
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // Deserializar la respuesta JSON
+                    JObject json = JObject.Parse(responseBody);
+
+                    // Obtener la lista de pilotos
+                    List<JToken> driverStandings = json["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"].Children().ToList();
+
+                    // Encontrar el piloto con más puntos
+                    int maxPoints = 0;
+                    string driverName = "";
+
+                    foreach (JToken driver in driverStandings)
+                    {
+                        int points = int.Parse(driver["points"].ToString());
+                        if (points > maxPoints)
+                        {
+                            maxPoints = points;
+                            driverName = driver["Driver"]["givenName"].ToString() + " " + driver["Driver"]["familyName"].ToString();
+                        }
+                    }
+
                     botMessages.Add(new Assistant()
                     {
                         Id = 0,
                         MsgType = MessageType.BotMessage,
-                        //personalizar respuesta
-                        ChatMessage = $"El piloto con más puntos es {driverName} con {maxPoints} puntos)"
+                        ChatMessage = $"El piloto con más puntos es {driverName} con {maxPoints} puntos"
                     });
                 }
-                //else if (userInput=="¿Cuál es el equipo con mas puntos?")
-                //{
-
-                //}
                 else
                 {
                     // Manejar el error de la solicitud HTTP aquí
@@ -95,13 +95,10 @@ public class ChatController : Controller
                     {
                         Id = 0,
                         MsgType = MessageType.BotMessage,
-                        //personalizar respuesta
-                        ChatMessage = "nores"
+                        ChatMessage = "Ha ocurrido un error al obtener la información de los pilotos."
                     });
                 }
             }
-
-            
         }
         else
         {
@@ -109,30 +106,112 @@ public class ChatController : Controller
             {
                 Id = 0,
                 MsgType = MessageType.BotMessage,
-                //personalizar respuesta
                 ChatMessage = respuestaChat
             });
         }
-
-        return View(botMessages);
+        return Json(botMessages);
     }
+
+    //public IActionResult _Chat()
+    //{
+    //    return PartialView("_Chat");
+    //}
+
+    //[HttpPost]
+    //public async Task<IActionResult> _Chat(string userInput)
+    //{
+    //    if (botMessages == null)
+    //    {
+    //        botMessages = new List<Assistant>();
+    //    }
+
+    //    botMessages.Add(new Assistant()
+    //    {
+    //        MsgType = MessageType.UserMessage,
+    //        ChatMessage = userInput
+    //    });
+
+    //    string respuestaChat = await lexClient.GetMessages("PDQFFBWOFP", "WBACPFOPCQ", "es_ES", "163y68h", userInput);
+
+    //    if (userInput.Equals("¿Cuál es el piloto con mas puntos?", StringComparison.OrdinalIgnoreCase))
+    //    {
+    //        using (HttpClient client = new HttpClient())
+    //        {
+    //            string url = "http://ergast.com/api/f1/current/driverstandings.json";
+    //            HttpResponseMessage response = await client.GetAsync(url);
+    //            if (response.IsSuccessStatusCode)
+    //            {
+    //                string responseBody = await response.Content.ReadAsStringAsync();
+
+    //                // Deserializar la respuesta JSON
+    //                JObject json = JObject.Parse(responseBody);
+
+    //                // Obtener la lista de pilotos
+    //                List<JToken> driverStandings = json["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"].Children().ToList();
+
+    //                // Encontrar el piloto con más puntos
+    //                int maxPoints = 0;
+    //                string driverName = "";
+
+    //                foreach (JToken driver in driverStandings)
+    //                {
+    //                    int points = int.Parse(driver["points"].ToString());
+    //                    if (points > maxPoints)
+    //                    {
+    //                        maxPoints = points;
+    //                        driverName = driver["Driver"]["givenName"].ToString() + " " + driver["Driver"]["familyName"].ToString();
+    //                    }
+    //                }
+
+    //                botMessages.Add(new Assistant()
+    //                {
+    //                    Id = 0,
+    //                    MsgType = MessageType.BotMessage,
+    //                    ChatMessage = $"El piloto con más puntos es {driverName} con {maxPoints} puntos"
+    //                });
+    //            }
+    //            else
+    //            {
+    //                // Manejar el error de la solicitud HTTP aquí
+    //                botMessages.Add(new Assistant()
+    //                {
+    //                    Id = 0,
+    //                    MsgType = MessageType.BotMessage,
+    //                    ChatMessage = "Ha ocurrido un error al obtener la información de los pilotos."
+    //                });
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {
+    //        botMessages.Add(new Assistant()
+    //        {
+    //            Id = 0,
+    //            MsgType = MessageType.BotMessage,
+    //            ChatMessage = respuestaChat
+    //        });
+    //    }
+
+    //    return PartialView("_Chat", botMessages);
+    //}
+
 }
 
 #region codigosuelto
-    //if (response != null && response.Interpretations != null && response.Interpretations.Count > 0)
-    //{
-    //    var interpretation = response.Interpretations[0];
-    //    //obtengo el nombre de la ranura y el valor asignado si la respuesta es de éxito
-    //    if (interpretation.Intent.Name == "OrderFlowers")
-    //    {
-    //        // Cojo los slots posibles
-    //        var slots = interpretation.Intent.Slots;
-    //        // Si mi slot es "FlowerType", obtengo su valor
-    //        if (slots.ContainsKey("FlowerType"))
-    //        {
-    //            string slotValue = slots["FlowerType"].Value.InterpretedValue;
-    //            ViewBag.A = slotValue;
-    //        }
-    //    }
-    //}
-    #endregion
+//if (response != null && response.Interpretations != null && response.Interpretations.Count > 0)
+//{
+//    var interpretation = response.Interpretations[0];
+//    //obtengo el nombre de la ranura y el valor asignado si la respuesta es de éxito
+//    if (interpretation.Intent.Name == "OrderFlowers")
+//    {
+//        // Cojo los slots posibles
+//        var slots = interpretation.Intent.Slots;
+//        // Si mi slot es "FlowerType", obtengo su valor
+//        if (slots.ContainsKey("FlowerType"))
+//        {
+//            string slotValue = slots["FlowerType"].Value.InterpretedValue;
+//            ViewBag.A = slotValue;
+//        }
+//    }
+//}
+#endregion
--- MvcChatBotAWS/wwwroot/js/site.js
+++ MvcChatBotAWS/wwwroot/js/site.js
@@ -2,3 +2,74 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    loadChatMessages(); // Cargar los mensajes del chat al cargar la página
+
+    $("#sendButton").click(function () {
+        var userInput = document.getElementById("userInput").value;
+
+        // Enviar la entrada del usuario al controlador a través de AJAX
+        $.ajax({
+            type: "POST",
+            url: "/Chat/_Chat",
+            data: { userInput: userInput },
+            success: function (data) {
+                // Actualizar la vista con los nuevos mensajes del chat
+                updateChatView(data);
+            },
+            error: function () {
+                // Manejar el error de la solicitud AJAX aquí
+            }
+        });
+
+        // Limpiar el campo de entrada del usuario
+        $("#userInput").val("");
+    });
+});
+
+function loadChatMessages() {
+    // Cargar los mensajes del chat al cargar la página
+    $.ajax({
+        type: "GET",
+        url: "/Chat/_Chat",
+        success: function (data) {
+            // Actualizar la vista con los mensajes del chat
+            updateChatView(data);
+        },
+        error: function () {
+            // Manejar el error de la solicitud AJAX aquí
+        }
+    });
+}
+
+function updateChatView(messages) {
+    //var chatContainer = $("#chatContainer");
+    var chatContainer = document.getElementById("chatContainer");
+    // Limpiar el contenedor del chat
+/*    chatContainer.empty();*/
+
+    // Recorrer los mensajes y agregarlos al contenedor del chat
+    for (var i = 0; i <= messages.length; i++) {
+        var message = messages[i];
+        var messageHtml = "";
+        if (message.msgType == 0) {
+            console.log(message.chatMessage);
+            // Construir el HTML para el mensaje del usuario
+            if (message.chatMessage != null) {
+                messageHtml = '<div class="d-flex flex-row p-3 user-mns">' +
+                    '<div class="bg-white-chat mr-2 p-3"><span class="text-muted">' + message.chatMessage + '</span></div>' +
+                    '<img src="https://img.icons8.com/color/48/000000/circled-user-male-skin-type-7.png" width="30" height="30">' +
+                    '</div>';
+            }
+        } else if (message.msgType == 1) {
+            // Construir el HTML para el mensaje del bot
+            messageHtml = '<div class="d-flex flex-row p-3">' +
+                '<img src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-7.png" width="30" height="30">' +
+                '<div class="chat ml-2 p-3"><span class="text-muted dot">' + message.chatMessage + '</span></div>' +
+                '</div>';
+        }
+        // Agregar el mensaje al contenedor del chat
+        chatContainer.innerHTML += messageHtml;
+    }
+}
